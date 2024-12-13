bf3d55e4d22b357f4faf9e502acb9f5847af0561
kapilAlphonic07 <kalphonic100@gmail.com>
2024-12-13 12:16:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/xyz', (req, res) => {
+    res.send('dev')
+})
+app.get('/login', (req, res ) => {
+    res.send(`<h1> send url </h1>`)
+}) 
+app.get('/youtube', (req, res ) => {
+    res.send(`<h2> This is youtube Video </h2>`)
+})
+app.listen(process.env.PORT , () => {
+  console.log(`Example app listening on port ${port}`)
+})
